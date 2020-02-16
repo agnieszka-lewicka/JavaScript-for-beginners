05c773dbd773bd409a731eb0a6c8a9b0d8d4307d
Agnieszka Lewicka <aelewicka@gmail.com>
2020-02-16 15:27:45 +0100

    test.js edited for THIS keyword

--- test.js
+++ test.js
@@ -1,24 +1,33 @@
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Aga";
-
-myCar.drive = function() {
-
-        console.log ("now driving");
-
-};
-
-myCar.drive();
-
 var myCar2 = {
 
             maxSpeed: 70,
             driver: "Aga Singer",
             drive: function(speed, time) {
-
                     console.log(speed * time);
+            },
+            test: function(){
+                    console.log(this);
             }
 
 };
 
+
+var myCar3 = {
+
+        maxSpeed: 70,
+        driver: "Zenek",
+        drive: function(speed, time) {
+                console.log(speed * time);
+        },
+        logDriver: function () {
+                console.log("the driver name is " + this.driver);
+
+        }
+
+};
+
+
+console.log(myCar2.maxSpeed);
 myCar2.drive(30, 3);
+myCar2.test();
+myCar3.logDriver();
