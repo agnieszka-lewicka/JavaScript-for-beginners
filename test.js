d04f3ef44a5d1248dc4af6fa6b11206a1e942e6a
Agnieszka Lewicka <aelewicka@gmail.com>
2020-02-16 16:12:06 +0100

    test.js edited for Constructor Functions

--- test.js
+++ test.js
@@ -1,33 +1,23 @@
-var myCar2 = {
+var Car = function(maxSpeed, driver){
 
-            maxSpeed: 70,
-            driver: "Aga Singer",
-            drive: function(speed, time) {
-                    console.log(speed * time);
-            },
-            test: function(){
-                    console.log(this);
-            }
-
-};
-
-
-var myCar3 = {
-
-        maxSpeed: 70,
-        driver: "Zenek",
-        drive: function(speed, time) {
+        this.maxSpeed = maxSpeed;
+        this.driver = driver;
+        this.drive = function(speed, time) {
                 console.log(speed * time);
-        },
-        logDriver: function () {
-                console.log("the driver name is " + this.driver);
+        };
+        this.logDriver = function() {
+                console.log("driver name is " + this.driver);
+        };
 
-        }
+}
 
-};
+var myCar = new Car(70, "Jesus");
+var myCar2 = new Car(50, "Agnieszka");
+var myCar3 = new Car(30, "J. Lo");
+var myCar4 = new Car(100, "Teddy Bear");
 
-
-console.log(myCar2.maxSpeed);
 myCar2.drive(30, 3);
-myCar2.test();
+myCar4.logDriver();
+console.log(myCar3.maxSpeed);
+
 myCar3.logDriver();
