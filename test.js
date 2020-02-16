01aeb4057ab0c0ab9e48107e06b7c932e4bdef26
Agnieszka Lewicka <aelewicka@gmail.com>
2020-02-16 17:15:56 +0100

    test.js modified for date object

--- test.js
+++ test.js
@@ -1,23 +1,6 @@
-var Car = function(maxSpeed, driver){
+var myDate = new Date();
+console.log(myDate);
 
-        this.maxSpeed = maxSpeed;
-        this.driver = driver;
-        this.drive = function(speed, time) {
-                console.log(speed * time);
-        };
-        this.logDriver = function() {
-                console.log("driver name is " + this.driver);
-        };
 
-}
-
-var myCar = new Car(70, "Jesus");
-var myCar2 = new Car(50, "Agnieszka");
-var myCar3 = new Car(30, "J. Lo");
-var myCar4 = new Car(100, "Teddy Bear");
-
-myCar2.drive(30, 3);
-myCar4.logDriver();
-console.log(myCar3.maxSpeed);
-
-myCar3.logDriver();
+var myPastDate = new Date(1986, 10, 30);
+console.log(myPastDate);
