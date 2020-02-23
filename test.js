d47881fd6eec4a5fe800f5d09769e8d1849e7b08
Agnieszka Lewicka <aelewicka@gmail.com>
2020-02-23 19:16:06 +0100

    test.js edited for setInterval

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-var myMessage = document.getElementById("message");
+var colourChanger = document.getElementById("colour-changer");
+var colours = ["red", "blue", "green", "pink"];
+var counter = 0;
 
-function showMessage(){
-    myMessage.className = "show";
+function changeColour (){
+
+    if (counter >= colours.length) {
+
+        counter = 0;        
+    }
+
+    colourChanger.style.background = colours [counter];
+    counter++;
 }
 
-setTimeout(showMessage, 3000);
+var myTimer = setInterval(changeColour, 3000);
 
+
+colourChanger.onclick = function() {
+
+    clearInterval(myTimer);
+    colourChanger.innerHTML = "Timer Stopped";
+};
